b2225f51d42af2111bdd030e6f95c3d656c6e447
astridyang <workyooll@gmail.com>
2024-04-22 23:15:09 +0900

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ function GameGrid({selectedGenre}:Props) {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
